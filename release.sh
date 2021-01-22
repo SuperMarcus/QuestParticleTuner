91726c2fbd2eb39df239f74dd9a8ad4e73e1ee15
Marcus Zhou <other.marcus@icloud.com>
2021-01-22 12:25:45 -0500

    Remove codegen from bmbf mod package

--- release.sh
+++ release.sh
@@ -2,4 +2,4 @@
 
 rm -f ParticleTuner.zip
 zip -j ParticleTuner.zip bmbfmod.json cover.gif
-find ./libs/arm64-v8a ! -name 'libmodloader.so' -type f -exec zip -j ParticleTuner.zip {} +
+find ./libs/arm64-v8a ! -name 'libmodloader.so' ! -name 'libcodegen_*.so' -type f -exec zip -j ParticleTuner.zip {} +
