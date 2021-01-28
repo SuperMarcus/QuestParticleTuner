a6ee92028315cbb5e4b18eb717d0ec9106bc6c32
Marcus Zhou <other.marcus@icloud.com>
2021-01-28 08:41:29 -0500

    Remove custom-type and questui from packages

--- release.sh
+++ release.sh
@@ -2,4 +2,9 @@
 
 rm -f ParticleTuner.zip
 zip -j ParticleTuner.zip bmbfmod.json cover.gif
-find ./libs/arm64-v8a ! -name 'libmodloader.so' ! -name 'libcodegen_*.so' -type f -exec zip -j ParticleTuner.zip {} +
+find ./libs/arm64-v8a \
+  ! -name 'libmodloader.so' \
+  ! -name 'libcodegen_*.so' \
+  ! -name 'libcustom-types.so' \
+  ! -name 'libquestui.so' \
+  -type f -exec zip -j ParticleTuner.zip {} +
