ffac3071f6917380a4cc942c95cc23775fa675c8
Marcus Zhou <other.marcus@icloud.com>
2021-03-21 13:56:08 -0400

    Default to non-standalone qmod package

--- release-qmod.sh
+++ release-qmod.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-rm -f ParticleTuner.qmod
-zip -j ParticleTuner.qmod mod.json
+TMP_DIR=`mktemp -d -t questmod`
+
+# Build standalone package
+rm -f ParticleTuner.qmod ParticleTuner.standalone.qmod
+cp mod.standalone.json "${TMP_DIR}/mod.json"
+zip -j ParticleTuner.standalone.qmod "${TMP_DIR}/mod.json"
 find ./libs/arm64-v8a \
   ! -name 'libmodloader.so' \
-  -type f -exec zip -j ParticleTuner.qmod {} +
+  -type f -exec zip -j ParticleTuner.standalone.qmod {} +
+
+# Build normal package
+zip -j ParticleTuner.qmod mod.json ./libs/arm64-v8a/libparticletuner.so
+
+# Cleanup
+rm -rf "${TMP_DIR}"
