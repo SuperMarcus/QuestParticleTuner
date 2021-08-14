0bfd851ba30434e4e84f94c18f7d85d077a31c9d
Marcus Zhou <other.marcus@icloud.com>
2021-08-14 12:48:23 -0400

    Update build scripts & qmod manifest
    - Add nightly workflow

--- release-qmod.sh
+++ release-qmod.sh
@@ -2,13 +2,19 @@
 
 #TMP_DIR=`mktemp -d -t questmod`
 
-rm -f ParticleTuner.qmod
+PKG_NAME=ParticleTuner.qmod
+
+if [[ "$NDK_DEBUG" = 1 ]]; then
+  PKG_NAME=ParticleTuner.Debug.qmod
+fi
+
+rm -f "$PKG_NAME"
 
 # Build normal package
-zip -j ParticleTuner.qmod mod.json
+zip -j "$PKG_NAME" mod.json cover.gif
 
 find ./libs/arm64-v8a \
   ! -name 'libmodloader.so' \
   ! -name 'libcodegen_*.so' \
   ! -name 'gdb*' \
-  -type f -exec zip -j ParticleTuner.qmod {} +
+  -type f -exec zip -j "$PKG_NAME" {} +
