f4a635fa5f7ea27563b167a7bc669d588266c4ba
bagusnl <dzakibagus@gmail.com>
2021-09-15 01:35:54 +0700

    Added PS script from template

---
+++ buildQMOD.ps1
@@ -0,0 +1,14 @@
+# Builds a .qmod file for loading with QuestPatcher
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+$ArchiveName = "particletuner_v1.3.1.qmod"
+$TempArchiveName = "particletuner_qmodtemp.zip"
+
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+Compress-Archive -Path "./libs/arm64-v8a/libparticletuner.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Move-Item $TempArchiveName $ArchiveName -Force
